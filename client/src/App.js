2fbeef9f8f81fa45ae26a4fb2bdc0336031f0fe7
Siddhartha010 <agarwalsiddharth010@gmail.com>
2025-11-12 00:48:55 +0530

    Remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ParticlesBackground from './components/ParticlesBackground';
-import API from './utils/api';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
